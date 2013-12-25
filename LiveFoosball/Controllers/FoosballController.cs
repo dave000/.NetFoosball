1216b47d301e4629250221e47a0c11c9d94e74cf
dave000 <dave@davesoft.org>
2013-12-25 10:58:05 +0100

    Initial checking
    MVC 5 frame with SignalR

---
+++ LiveFoosball/Controllers/FoosballController.cs
@@ -0,0 +1,20 @@
+﻿using LiveFoosball.SignalR;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LiveFoosball.Controllers
+{
+    public class FoosballController : ApiController
+    {
+        [ActionName("start"), HttpGet]
+        public void StartGame()
+        {
+            GlobalHost.ConnectionManager.GetHubContext<FoosballHub>().Clients.All.canStartGame();
+        }
+    }
+}
---
+++ LiveFoosball/SignalR/FoosballHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace LiveFoosball.SignalR
+{
+    public enum Team
+    {
+        Blue = 1,
+        Red = 2
+    }
+
+    public class FoosballHub : Hub
+    {
+        public void Goal(Team team)
+        {
+            Clients.All.goal(team);
+        }
+
+        public void StartGame()
+        {
+            Clients.All.canStartGame();
+        }
+    }
+}
